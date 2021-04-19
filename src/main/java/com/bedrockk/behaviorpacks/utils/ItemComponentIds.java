25fe67d5f0f09e965060a89afaad2fef705992a4
EmreTr1 <emre.yavuz.842@gmail.com>
2021-04-19 19:13:39 +0300

    Added Item and Block component ids

---
+++ src/main/java/com/bedrockk/behaviorpacks/utils/ItemComponentIds.java
@@ -0,0 +1,67 @@
+package com.bedrockk.behaviorpacks.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+@SuppressWarnings("unused")
+public class ItemComponentIds {
+
+    public static final String USE_DURATION = "minecraft:use_duration";
+    public static final String FOOD = "minecraft:food";
+    public static final String HAND_EQUIPPED = "minecraft:hand_equipped";
+    public static final String STACKED_BY_DATA = "minecraft:stacked_by_data";
+    public static final String FOIL = "minecraft:foil";
+    public static final String SEED = "minecraft:seed";
+    public static final String MAX_STACK_SIZE = "minecraft:max_stack_size";
+    public static final String BLOCK = "minecraft:block";
+    public static final String CAMERA = "minecraft:camera";
+    public static final String MAX_DAMAGE = "minecraft:max_damage";
+    public static final String DOOR = "minecraft:door";
+    public static final String CREATIVE_CATEGORY = "minecraft:creative_category";
+    public static final String SIGN = "minecraft:sign";
+    public static final String ICON = "minecraft:icon";
+    public static final String ALLOW_OFF_HAND = "minecraft:allow_off_hand";
+    public static final String PROJECTILE = "minecraft:projectile";
+    public static final String BED = "minecraft:bed";
+    public static final String BLOCK_PLACER = "minecraft:block_placer";
+    public static final String LIQUID_CLIPPED = "minecraft:liquid_clipped";
+    public static final String ENTITY_PLACER = "minecraft:entity_placer";
+    public static final String ENCHANTABLE = "minecraft:enchantable";
+    public static final String FRAME_COUNT = "minecraft:frame_count";
+    public static final String ANIMATES_IN_TOOLBAR = "minecraft:animates_in_toolbar";
+    public static final String SHOOTER = "minecraft:shooter";
+    public static final String DAMAGEABLE = "minecraft:damageable";
+    public static final String BUCKET = "minecraft:bucket";
+    public static final String MIRRORED_ART = "minecraft:mirrored_art";
+    public static final String REQUIRES_INTERACT = "minecraft:requires_interact";
+    public static final String CARROT_ON_A_STICK = "minecraft:carrot_on_a_stick";
+    public static final String ARMOR = "minecraft:armor";
+    public static final String WEARABLE = "minecraft:wearable";
+    public static final String IGNORES_PERMISSION = "minecraft:ignores_permission";
+    public static final String COOLDOWN = "minecraft:cooldown";
+    public static final String COAL = "minecraft:coal";
+    public static final String MINING_SPEED = "minecraft:mining_speed";
+    public static final String DAMAGE = "minecraft:damage";
+    public static final String DIGGER = "minecraft:digger";
+    public static final String USE_ON = "minecraft:use_on";
+    public static final String CAN_DESTROY_IN_CREATIVE = "minecraft:can_destroy_in_creative";
+    public static final String DYE_POWDER = "minecraft:dye_powder";
+    public static final String FERTILIZER = "minecraft:fertilizer";
+    public static final String THROWABLE = "minecraft:throwable";
+    public static final String MAP = "minecraft:map";
+    public static final String FIRE_CHARGE = "minecraft:fire_charge";
+    public static final String FISHING_ROD = "minecraft:fishing_rod";
+    public static final String FLINT_AND_STEEL = "minecraft:flint_and_steel";
+    public static final String BOTTLE = "minecraft:bottle";
+    public static final String LEAD = "minecraft:lead";
+    public static final String DYEABLE = "minecraft:dyeable";
+    public static final String POTION = "minecraft:potion";
+    public static final String NAME_TAG = "minecraft:name_tag";
+    public static final String EXPLODABLE = "minecraft:explodable";
+    public static final String SHOULD_DESPAWN = "minecraft:should_despawn";
+    public static final String HANGING_ENTITY = "minecraft:hanging_entity";
+    public static final String SADDLE = "saddle";
+    public static final String SHEARS = "minecraft:shears";
+    public static final String SKULL = "minecraft:skull";
+    public static final String MOB_PLACER = "minecraft:mob_placer";
+}
