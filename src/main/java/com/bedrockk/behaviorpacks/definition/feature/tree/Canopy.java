d24575285a5b95aae1966c0e6cf25a782c3437de
EmreTr1 <emre.yavuz.842@gmail.com>
2021-04-20 05:57:28 +0300

    upload unversioned file

---
+++ src/main/java/com/bedrockk/behaviorpacks/definition/feature/tree/Canopy.java
@@ -0,0 +1,25 @@
+package com.bedrockk.behaviorpacks.definition.feature.tree;
+
+import com.bedrockk.behaviorpacks.type.BlockReference;
+import com.bedrockk.behaviorpacks.type.Chance;
+import com.bedrockk.behaviorpacks.type.JsonList;
+import com.bedrockk.behaviorpacks.type.Range;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Canopy {
+    private Range canopyOffset;
+    private Integer minWidth;
+    private Slope canopySlope;
+    private JsonList<Chance> variationChance;
+    private BlockReference leafBlock;
+    private Decoration canopyDecoration;
+
+    @Data
+    public static class Slope {
+        private int rise;
+        private int run;
+    }
+}
